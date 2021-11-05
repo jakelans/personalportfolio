56fff2ee742c34601534709268b9342cea87dc64
jakelans <91145742+jakelans@users.noreply.github.com>
2021-11-04 19:27:00 -0600

    css styling

--- films/sw-films.js
+++ films/sw-films.js
@@ -1,26 +1,27 @@
-import { films } from "../data/films.js";
-import { getLastNumber } from "../utils/index.js";
+import { films } from '../data/films.js'
+import { getLastNumber } from '../utils/index.js'
 
-let filmList = document.querySelector("#filmList");
+let filmList = document.querySelector('#filmList')
 
 for (let i = 0; i < films.length; i++) {
-  let figure = document.createElement("figure");
-  let figImg = document.createElement("img");
-  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`;
-  let figCaption = document.createElement("figcaption");
+  let figure = document.createElement('figure')
+  let figImg = document.createElement('img')
+  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+  let figCaption = document.createElement('figcaption')
 
-  const foundFilm = films.find((film) => {
-    const convertedString = parseInt(getLastNumber(film.url), 10);
-    return convertedString === i + 1;
-  });
+  
+  const foundFilm = films.find(film => {
+    const convertedString = parseInt(getLastNumber(film.url), 10)
+    return convertedString === (i + 1)
+  })
 
-  console.log(foundFilm);
+  console.log(foundFilm)
 
-  figCaption.textContent = foundFilm.title;
+  figCaption.textContent = foundFilm.title
+  
+  figure.appendChild(figImg)
+  figure.appendChild(figCaption)
+  filmList.appendChild(figure)
+ }
 
-  figure.appendChild(figImg);
-  figure.appendChild(figCaption);
-  filmList.appendChild(figure);
-}
-
-// add a function later that will get the URL property and use Array.find() to match titles with posters
+ // add a function later that will get the URL property and use Array.find() to match titles with posters
--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,9 +1,18 @@
-import { starships } from "../data/starships.js";
-import { getLastNumber } from "../utils/index.js"
+import { starships } from "../data/starships.js"
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.displaySection')
+
+const modal = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+const missingMessage = document.querySelector('.missingMessage')
+
+closeButton.addEventListener('click', () => modal.classList.toggle('is-active'))
+modalBackground.addEventListener('click', () => modal.classList.toggle('is-active'))
 
 function populateNav(starships) {
   starships.forEach(starship => {
@@ -23,9 +32,15 @@ function populateNav(starships) {
 populateNav(starships)
 
 function populateShipView(shipData) {
+  removeChildren(shipView)
   console.log(`Thanks for clicking on ${shipData.name}`)
   let shipImage = document.createElement('img')
   let shipNum = getLastNumber(shipData.url)
-  shipImage.src = `https:starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+  shipImage.addEventListener('error', () => {
+    shipImage.hidden = true
+    modal.classList.toggle('is-active')
+    missingMessage.textContent = `The ship ${shipData.name} has been canceled.`
+  })
   shipView.appendChild(shipImage)
 }
