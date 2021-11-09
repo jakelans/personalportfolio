56a6f5f7d22df7c0b30b2a655f3f1e35f044855e
jakelans <91145742+jakelans@users.noreply.github.com>
2021-11-09 14:02:43 -0700

    edits for star wars on main page

--- congress/congress.js
+++ congress/congress.js
@@ -4,12 +4,23 @@ const sentaorDiv = document.querySelector('.senators')
 
 function SimplifiedSenators(senatorArray) {
     return senatorArray.map(senator => {
-        let middleName = senator.middle_name ? `${senator.middle_name}`: ``
+        let middleName = senator.middle_name ? `${senator.middle_name} ` : ` `
         return {
             id: senator.id,
-            name: `${senator.first_name} ${middleNName} ${senator.last_name}`,
+            name: `${senator.first_name}${middleNName}${senator.last_name}`,
             party: senator.party
+            imgUrl:
         }
     })
 }
 
+console.log(SimplifiedSenators(senators))
+
+function populateSenatorDiv(simpleSenators) {
+    simpleSenators.forEach(senator => {
+        const senFigure = document.createElement('img')
+        const figCaption = document.createElement('figcaption')
+
+        figImg.src = 
+    })
+}
