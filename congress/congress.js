9009e0f49882994a4d1c44a2dff0d3273f6f41f0
jakelans <91145742+jakelans@users.noreply.github.com>
2021-11-11 10:04:11 -0700

    updated congress folder

--- congress/congress.js
+++ congress/congress.js
@@ -1,26 +1,33 @@
-import { senators  } from '../data/senators.js'
+import { senators } from '../data/senators.js'
 
-const sentaorDiv = document.querySelector('.senators')
+const senatorDiv = document.querySelector('.senators')
 
 function SimplifiedSenators(senatorArray) {
-    return senatorArray.map(senator => {
-        let middleName = senator.middle_name ? `${senator.middle_name} ` : ` `
-        return {
-            id: senator.id,
-            name: `${senator.first_name}${middleNName}${senator.last_name}`,
-            party: senator.party
-            imgUrl:
-        }
-    })
+  return senatorArray.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    return {
+      id: senator.id,
+      name: `${senator.first_name}${middleName}${senator.last_name}`,
+      party: senator.party,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
+    }
+  })
 }
 
-console.log(SimplifiedSenators(senators))
+populateSenatorDiv(SimplifiedSenators(senators))
 
 function populateSenatorDiv(simpleSenators) {
-    simpleSenators.forEach(senator => {
-        const senFigure = document.createElement('img')
-        const figCaption = document.createElement('figcaption')
+  simpleSenators.forEach(senator => {
+    const senFigure = document.createElement('figure')
+    const figImg = document.createElement('img')
+    const figCaption = document.createElement('figcaption')
 
-        figImg.src = 
-    })
+    figImg.src = senator.imgURL
+    figCaption.textContent = senator.name
+
+    senFigure.appendChild(figImg)
+    senFigure.appendChild(figCaption)
+    senatorDiv.appendChild(senFigure)
+  })
+
 }
